706fa69b087dbf362ded00dd7ad54d088928f24e
Lily Le <lmlt8180@gmail.com>
2024-11-27 20:40:42 -0500

    change period to exclamation mark to keep it consistent with other slash command descriptions

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from "discord.js";
 export const command = {
     data: new SlashCommandBuilder()
         .setName("user")
-        .setDescription("Provides information about the user."),
+        .setDescription("Provides information about the user!"),
     async execute(interaction) {
         await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
     },
